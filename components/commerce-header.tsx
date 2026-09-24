f537859d8317b6f0c6ff683457865a65ae2a5d31
sardarpharmacy87-lang <sardarpharmacy87@gmail.com>
2026-09-24 15:30:23 +0500

    Add restored sections to site navigation

--- components/commerce-header.tsx
+++ components/commerce-header.tsx
@@ -60,6 +60,9 @@ export function CommerceHeader() {
           <Link href="/products" onClick={() => setOpen(false)}>Products</Link>
           <Link href="/#heritage" onClick={() => setOpen(false)}>Our history</Link>
           <Link href="/#workshop" onClick={() => setOpen(false)}>Workshop</Link>
+          <Link href="/#gallery" onClick={() => setOpen(false)}>Gallery</Link>
+          <Link href="/#process" onClick={() => setOpen(false)}>How we work</Link>
+          <Link href="/#contact" onClick={() => setOpen(false)}>Contact</Link>
           <Link href="/sign-in" onClick={() => setOpen(false)}>My account</Link>
           <Link className="nav-quote-link" href="/customer/enquiries/new" onClick={() => setOpen(false)}>
             Custom enquiry
