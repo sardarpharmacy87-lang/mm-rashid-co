3433df6dbe31a05cf7d7af78ce3eedd7ec4466fd
Alimurtaza93 <belovedson93@gmail.com>
2026-09-25 12:48:47 +0500

    Use original MM Rashid logo in header

--- components/brand.tsx
+++ components/brand.tsx
@@ -14,7 +14,7 @@ export function Brand({ footer = false }: BrandProps) {
     >
       <span className="brand-logo">
         <Image
-          src="/favicon.svg"
+          src="/mm-rashid-logo.jpg"
           alt="MM Rashid & Co. logo"
           width={58}
           height={58}
