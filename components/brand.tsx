16ed83ce4362436dd1edf66f63600940d4baac5b
Alimurtaza93 <belovedson93@gmail.com>
2026-09-25 12:50:33 +0500

    Match header logo to hero emblem

--- components/brand.tsx
+++ components/brand.tsx
@@ -14,7 +14,7 @@ export function Brand({ footer = false }: BrandProps) {
     >
       <span className="brand-logo">
         <Image
-          src="/mm-rashid-logo.jpg"
+          src="/mm-rashid-logo.png"
           alt="MM Rashid & Co. logo"
           width={58}
           height={58}
